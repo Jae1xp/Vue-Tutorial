5759c699d85b8c708c37b87af1a0a5e5829443db
Jae Son <>
2018-12-10 17:50:45 -0500

    small example of conditional code

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
 new Vue({
   el: '#app',
   data: {
-    name: '',
-    age: 20,
-    x: 0,
-    y: 0,
-    a: 0,
-    b: 0
+    // name: '',
+    // age: 20,
+    // x: 0,
+    // y: 0,
+    // a: 0,
+    // b: 0
+    error: false,
+    success: false
   },
   methods: {
-    greet: function(time) {
-      return 'Good ' + time + ' ' + this.name;
-    },
-    addYear: function(inc) {
-      this.age += inc;
-    },
-    subtractYear: function(dec) {
-      this.age -= dec;
-    },
-    updateXY: function(event) {
-      this.x = event.offsetX;
-      this.y = event.offsetY; 
-    },
-    click: function() {
-      alert('You clicked me!');
-    },
+    // greet: function(time) {
+    //   return 'Good ' + time + ' ' + this.name;
+    // },
+    // addYear: function(inc) {
+    //   this.age += inc;
+    // },
+    // subtractYear: function(dec) {
+    //   this.age -= dec;
+    // },
+    // updateXY: function(event) {
+    //   this.x = event.offsetX;
+    //   this.y = event.offsetY; 
+    // },
+    // click: function() {
+    //   alert('You clicked me!');
+    // },
   },
-  computed: {
-    addToA: function() {
-      console.log('adding to A');
-      return this.a + this.age;
-    },
-    addToB: function() {
-      console.log('adding to B');
-      return this.b + this.age;
-    }
-  }
+  // computed: {
+  //   addToA: function() {
+  //     console.log('adding to A');
+  //     return this.a + this.age;
+  //   },
+  //   addToB: function() {
+  //     console.log('adding to B');
+  //     return this.b + this.age;
+  //   }
+  // }
 });
